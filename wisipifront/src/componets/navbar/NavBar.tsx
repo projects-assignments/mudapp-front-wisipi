e7b5db4497bb1c6ca9c15d30272400464b4d78ee
wilpipe <residentsbcn@gmail.com>
2024-03-03 20:09:02 +0100

    avatar Wilfredo/Inno

--- wisipifront/src/componets/navbar/NavBar.tsx
+++ wisipifront/src/componets/navbar/NavBar.tsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/logo2.png'
+import Avatar from '../../assets/avatar-Icon.png'
 
 import "./navBar.css"
 
@@ -15,14 +16,14 @@ export const NavBar = (props: Props) => {
     <Navbar className="custom-navbar" variant="dark">
       <Container>
         <Navbar.Brand href="/" className="text-light">
-          <img alt="logo" src={logo} width="250" height="" className="Logo" />{" "}
+          <img alt="logo" src={logo} width="150" height="" className="Logo" />{" "}
         </Navbar.Brand>
         
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="/Login">Login</Nav.Link>
+            <Nav.Link href="/Login">
+            <img alt="avatar" src={Avatar} width="40" height="" className="avatar" />{" "}
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
