78d05c4b018cd1eb84fb1f72c304e6287f6b15c2
Pamela Baez <pamelabaez182@gmail.com>
2024-03-04 19:15:29 +0100

    searchbar actualizado

--- wisipifront/src/componets/header/Header.tsx
+++ wisipifront/src/componets/header/Header.tsx
@@ -1,40 +1,47 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import heroImage from '../../assets/heroImage.svg';
 import heroImageMobile from '../../assets/heroImageMobile.svg'
 import './header.css';
-
+import { SearchBar } from '../searchbar/Search.tsx';
 
 // type HeaderProps = {}
 
 const Header = () => {
-const [isMobile, setIsMobile] =useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-useEffect(() =>{
-  const updateSize = () => {
-    setIsMobile(window.innerWidth <768);
-  }
+  useEffect(() => {
+    const updateSize = () => {
+      setIsMobile(window.innerWidth < 768);
+    }
 
-  window.addEventListener('resize', updateSize);
-  updateSize
-  return () => window.removeEventListener('resize', updateSize)
-},[ ]);
- 
+    window.addEventListener('resize', updateSize);
+    updateSize
+    return () => window.removeEventListener('resize', updateSize)
+  }, []);
+
   return (
     <Container fluid className="hero-container">
-    <Row>
+      <Row>
         <Col className="position-relative">
-            <div className="hero-text">
-                <p>Bienvenidos a</p>
-                <h1>MuddAPP</h1>
-                <p>mudanzas a un click!</p>
-            </div>
-            <Image src={isMobile ? heroImageMobile : heroImage} alt="HeroImage" fluid />
-           
+          <div className="hero-text">
+            <p>Bienvenidos a</p>
+            <h1>MuddAPP</h1>
+            <p>mudanzas a un click!</p>
+          </div>
+          <Image src={isMobile ? heroImageMobile : heroImage} alt="HeroImage" fluid />
+          <div className={isMobile ? "position-relative mt-5 search-container-mobile" : "position-absolute bottom-0 start-0 w-100 mb-4 mx-4"} style={{ maxWidth: '300px' }}>
+            <SearchBar />
+          </div>
+
         </Col>
-    </Row>
-</Container>
+      </Row>
+    </Container>
+
   );
 }
 
+
 export default Header;
+
+
---
+++ wisipifront/src/componets/searchbar/Search.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { TbArrowBigRightFilled } from "react-icons/tb";
+
+export const SearchBar = () => {
+    return (
+        <Form className="d-flex flex-column">
+            <div className="mb-3" style={{ marginLeft: '65px' }}>
+                <Form.Label><strong>Me mudo desde</strong></Form.Label>
+                <Form.Control
+                    type="search"
+                    placeholder="Introduce tu código postal"
+                    aria-label="Search"
+                />
+            </div>
+
+            <div className="mb-3" style={{ marginLeft: '65px' }}>
+                <Form.Label><strong>Me mudo a</strong></Form.Label>
+                <div className="d-flex align-items-center">
+                    <Form.Control
+                        type="search"
+                        placeholder="Introduce tu dirección"
+                        aria-label="Search"
+                        className="mb-3"
+                    />
+
+                </div>
+                <Button variant="outline-success" className="rounded-circle d-flex align-items-center justify-content-center" style={{ width: '50px', height: '50px', backgroundColor: '#0D6E6E', marginLeft: '65px' }}>
+                    <TbArrowBigRightFilled style={{ backgroundColor: '0D6E6E', color: '#fff' }} />
+                </Button>
+            </div>
+        </Form>
+
+    )
+}
