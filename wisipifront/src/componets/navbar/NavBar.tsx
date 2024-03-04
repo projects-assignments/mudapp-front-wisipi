2bd13d29a8612da0e7c117e234c9ce78b3467b1f
wilpipe <residentsbcn@gmail.com>
2024-03-04 20:44:57 +0100

    modal login updating inno/wil

--- wisipifront/src/componets/navbar/NavBar.tsx
+++ wisipifront/src/componets/navbar/NavBar.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/logo2.png'
-import Avatar from '../../assets/avatar-Icon.png'
+
 
 import "./navBar.css"
 import LoginForm from '../modals/Modal-Login';
