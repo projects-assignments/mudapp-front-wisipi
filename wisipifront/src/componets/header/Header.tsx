1b37c6505a3fdc05356ac32864555a1be210ee79
Isadora Matias <izzadora@outlook.com>
2024-03-04 12:34:42 +0100

    Update Header component for responsive image

--- wisipifront/src/componets/header/Header.tsx
+++ wisipifront/src/componets/header/Header.tsx
@@ -1,10 +1,25 @@
+import {useState, useEffect} from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import heroImage from '../../assets/heroImage.svg';
+import heroImageMobile from '../../assets/heroImageMobile.svg'
 import './header.css';
 
-type HeaderProps = {}
 
-function Header(_props: HeaderProps) {
+// type HeaderProps = {}
+
+const Header = () => {
+const [isMobile, setIsMobile] =useState(false);
+
+useEffect(() =>{
+  const updateSize = () => {
+    setIsMobile(window.innerWidth <768);
+  }
+
+  window.addEventListener('resize', updateSize);
+  updateSize
+  return () => window.removeEventListener('resize', updateSize)
+},[ ]);
+ 
   return (
     <Container fluid className="hero-container">
     <Row>
@@ -14,7 +29,8 @@ function Header(_props: HeaderProps) {
                 <h1>MuddAPP</h1>
                 <p>mudanzas a un click!</p>
             </div>
-            <Image src={heroImage} alt="HeroImage" fluid />
+            <Image src={isMobile ? heroImageMobile : heroImage} alt="HeroImage" fluid />
+           
         </Col>
     </Row>
 </Container>
