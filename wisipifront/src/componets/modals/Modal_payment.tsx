cc65451ff6ad26a84fa54b88b52b73686e022554
sefogi <sebast18@gmail.com>
2024-03-05 15:58:51 +0100

    actualizacion payment

--- wisipifront/src/componets/modals/Modal_payment.tsx
+++ wisipifront/src/componets/modals/Modal_payment.tsx
@@ -1,9 +1,13 @@
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import icon1 from "..//..//assets/ic_baseline-paypal.png"
+import icon2 from "..//..//assets/Bizum 1.png"
+import icon3 from "..//..//assets/mingcute_card-pay-fill.png"
 
 type Props = {};
 
@@ -15,52 +19,62 @@ export const Modal_payment = (props: Props) => {
     >
       <Container>
         <Modal.Dialog>
-          <Modal.Header  closeButton style={{ backgroundColor: "#479CB7" }}>
-            <Modal.Title  className="text-light"><h2 style={{ textAlign: "center" }}>Payment</h2></Modal.Title>
+          <Modal.Header closeButton style={{ backgroundColor: "#479CB7" }}>
+            <Modal.Title className="text-light">
+              <h2>Realiza tu pago</h2>
+            </Modal.Title>
           </Modal.Header>
 
           <Modal.Body style={{ backgroundColor: "#479CB7" }}>
-            <Form>
-              <Row>
-                <Col sm={5}>
-                  <Form.Group className="mb-3" controlId="formModalUbicacion">
-                    <Form.Label>Ubicación</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Ingresa tu ubicación"
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={5}>
-                  <Form.Group className="mb-3" controlId="formModalUbicacion">
-                    <Form.Label>Capacidad de carga</Form.Label>
-                    <Form.Control type="text" placeholder="Capacidad" />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={5}>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Destino</Form.Label>
-                    <Form.Control type="Text" placeholder="Destino" />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={5}>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Horario</Form.Label>
-                    <Form.Control type="Text" placeholder="Horario" />
-                  </Form.Group>
-                </Col>
-              </Row>
-            </Form>
+            <Row className="g-2">
+              <Col md>
+                <FloatingLabel
+                  controlId="floatingInputGrid"
+                  label="Ubicacion"
+                >
+                  <Form.Control type="text" placeholder="lugar" />
+                </FloatingLabel>
+              </Col>
+              <Col md>
+                <FloatingLabel controlId="floatingInputGrid"
+                  label="Destino"
+                >
+                  <Form.Control type="text" placeholder="name@example.com" />
+                  
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <br></br>
+            <Row className="g-2">
+              <Col md>
+                <FloatingLabel
+                  controlId="floatingInputGrid"
+                  label="Capacidad"
+                >
+                  <Form.Control type="number" placeholder="Tipo de transporte" />
+                </FloatingLabel>
+              </Col>
+              <Col md>
+                   <FloatingLabel
+                  controlId="floatingInputGrid"
+                  label="Horario"
+                >
+                  <Form.Control type="time" placeholder="fecha" />
+                </FloatingLabel>
+              </Col>
+            </Row>
           </Modal.Body>
+          <div style={{backgroundColor: "#479CB7"}}>
+      <Row>
+        
+        <Col><img src={icon1} width={100} style={{padding:10}}/></Col>
+        <Col ><img src={icon2} width={150}/></Col>
+        <Col><img src={icon3} width={100}/></Col>
+      </Row>
+          </div>
 
           <Modal.Footer style={{ backgroundColor: "#479CB7" }}>
-            <Button  variant="light">Confirmar</Button>
+            <Button variant="light">Confirmar</Button>
           </Modal.Footer>
         </Modal.Dialog>
       </Container>
