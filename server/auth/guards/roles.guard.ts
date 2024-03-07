d1a54030efbcef939e977fecf3d6da74bba83196
wilpipe <residentsbcn@gmail.com>
2024-03-07 09:49:49 +0100

    fixing merging problems

---
+++ server/auth/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../constants/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean 
+
+  {
+    const requiredRoles = this.reflector.getAllAndOverride<Role>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    
+    if(!requiredRoles){
+
+      return true;
+    }
+ 
+    const { user } = context.switchToHttp().getRequest();
+    console.log(user);
+    
+    if(user.admin) return true;
+    if (!user.admin) throw new UnauthorizedException();
+}
+}
